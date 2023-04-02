f8e808b8862038b7125ebd79af3afd780d3d6d39
codinger-cutie <124452291+codinger-cutie@users.noreply.github.com>
2023-04-02 21:15:31 +0200

    Added project files

---
+++ script.js
@@ -0,0 +1,51 @@
+const colorPicker = document.querySelector("#color-pick");
+
+colorPicker.addEventListener("input", updateFirst, false);
+colorPicker.addEventListener("change", watchColorPicker, false);
+
+function updateFirst(event) {
+  const pageContent = document.querySelector(".page-content");
+  if (pageContent) {
+    pageContent.style.backgroundColor = event.target.value;
+  }
+}
+
+function watchColorPicker(event) {
+  document.querySelectorAll(".pageContent").forEach((pageContent) => {
+    pageContent.style.backgroundColor = event.target.value;
+  });
+}
+
+const testBtn = document.getElementById("bgcolor-box");
+let box = document.querySelector(".page-content");
+
+let p1 = document.createElement("p");
+p1.innerHTML = '<p>Hello!</p>';
+
+box.appendChild(p1);
+
+
+const h1AddBtn = document.querySelector("#test-add");
+const testh1 = document.createElement("h1");
+testh1.innerHTML = '<h1>Test h1</h1>';
+
+function h1test() {
+  box.appendChild(testh1);
+}
+
+h1AddBtn.addEventListener('click', h1test);
+
+/* test code below */
+const selectElement = Document.getElementById("add-element-select");
+
+const test = document.getElementById("test");
+
+const addBox = document.getElementsByClassName("add-box");
+
+addBox.addEventListener("click", displayValue);
+
+function displayValue() {
+   let val = documentGetElementById("add-element-select").value();
+   test.innerHTML = val;
+}
+ /* test code above */
